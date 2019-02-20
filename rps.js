3247ca5100811f8e7970baa164a375cc2282272a
aphilliard <46166005+aphilliard@users.noreply.github.com>
2019-02-20 11:54:33 -0500

    Add files via upload

--- rps.js
+++ rps.js
@@ -1,5 +1,6 @@
-function playRound(p, c) {
+var score = 0;
 
+function playRound(p, c) {
     console.log("Computer selects " + computerSelection);
     console.log("Player selects " + playerSelection);
 
@@ -8,9 +9,11 @@ function playRound(p, c) {
     }
     else if ((p === "rock" && c === "scissors") || (p === "paper" && c === "rock")) {
         console.log("Player wins!");
+	score++;
     }
     else {
         console.log("Computer wins!");
+	score--;
     }
 }
 
@@ -28,5 +31,16 @@ for (i = 0; i < 5; i++) {
 
     playRound(playerSelection, computerSelection)
 }
+console.log(score);
 
+if (score === 0) {
+    console.log("The game was a tie.");
+}
+else if (score > 0) {
+    console.log("The player wins!");
+}
+else {
+    console.log("The computer wins!");
+}
+   
  
